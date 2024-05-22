64c441decb63c1bfb7b25e3e1576ae78e4365134
noumanmani <168138758+noumanmani@users.noreply.github.com>
2024-05-22 05:45:20 +0500

    new data

--- script.js
+++ script.js
@@ -1,62 +1,42 @@
- const addnumber =(...one)=>{
-  return one
-}
-// console.log(addnumber(48,98,90,65,98));
-// function plasnumber(...saith){
-//     return saith
-// // }
-// // console.log('plus number',plasnumber (889,98765,109));
-// // console.log('okay');
-
-
-// // const newarry = [79,99,67,745];
-// // function arrr (newa){
-// //    return newa [1,2,3]
-// // }
-// // console.log(newarry,arrr(newarry));
-// // let nome=()=>{
-// //     let a =90;
-// //     if(
-// //         a<=100
-// //     ){
-// //         console.log('90trye');
-// //     }
-// let a =78;
-// let n=89;
-// let c=99;
-// // function nouman (x,u){
-// //     return x+u
-// // }
-// // // console.log('right',nouman(a,n));
-
-// // // let nome = ()=>{}
-
-
-
-// // // let nouman = new Promise(
-// // //     ()=>{
-// // //         resolve,reject
-// // //     }
-// // // )
-// // console.log('asad');
-// // // console.log('ahsan');
-// // // console.log('hamza');
-
-// // // function hlo(){
-// // //     console.log('hello');
-// // // }setTimeout((hlo) => {
-    
-// // // },2000);
-// // function hlo(){
-// //     console.log('nouman');
-// // }setTimeout(hlo,1000)
-
-
-// console.log('nomme');
-
-// setTimeout(
-//     ()=>{
-//         console.log('qasam');
-//     },2000,
-// )
-// console.log('shoib');
+function hryap(goodmorning,oftternoon){
+    setTimeout(() => {
+      console.log('happy your day ',goodmorning);
+      if(oftternoon ){
+        oftternoon();
+      }  
+    }, 2000);
+};
+hryap(
+    1,()=>{
+        hryap(
+            2,()=>{
+                hryap(
+                    3,()=>{
+                        hryap(4,
+                            ()=>{
+                                hryap(5,
+                                    ()=>{
+                                        hryap(6,
+                                            ()=>{
+                                                hryap(4,
+                                                    ()=>{
+                                                        hryap(99,
+                                                            ()=>{
+                                                            hryap(89
+                                                            )
+                                                        }
+                                                      )
+                                                    }
+                                                )
+                                            }
+                                        )
+                                    }
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        )
+    }
+)
